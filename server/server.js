7298638105fc6a6dc8b77d8424ce498bcd377a2b
liverday <vitor.medeiro10@gmail.com>
2018-06-20 01:55:57 -0300

    post and get resources ready

---
+++ server/server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const { ObjectID } = require('mongodb');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+
+const port = process.env.PORT || 3000
+var server = express();
+
+server.use(bodyParser.json());
+server.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text,
+        completed: req.body.completed
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+server.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    })
+})
+
+server.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send({ code: 'ID_NOT_VALID', message: 'ID not valid, try again with a new one' })
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(400).send({ code: 'ID_NOT_FOUND', message: 'ID not found, try again with a new one' })
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send({});
+    })
+})
+
+server.disable('etag');
+server.listen(port, () => {
+    console.log(`Started on port ${port}`)
+});
+
+module.exports = { server };
---
+++ server/tests/server.test.js
@@ -0,0 +1,70 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const { server } = require('../server');
+const { Todo } = require('../models/todo')
+
+const todos = [{
+    text: 'First dummy todo'
+}, {
+    text: 'Second dummy todo'
+}];
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }, (e) => done(e)).then(() => done())
+});
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(server)
+            .post('/todos')
+            .send({ text })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text)
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find({ text }).then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should not create todo with invalid body data', (done) => {
+        request(server)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .expect((err, res) => {
+                if (err)
+                    return done(err);
+            })
+
+        Todo.find().then((todos) => {
+            expect(todos.length).toBe(2);
+            done();
+        }).catch((e) => done(e));
+    })
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(server)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+});
