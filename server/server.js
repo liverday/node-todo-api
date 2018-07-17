055f4faac6978f6a423cbf9862e626331618f24c
liverday <vitor.medeiro10@gmail.com>
2018-07-17 00:10:30 -0300

    expose headers

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth");
+  res.header("Access-Control-Expose-Headers", "x-auth");
   next();
 });
 
